9434be7b93f21d76efb26246520e7d8c027f42b4
Tristan Muller <tristanmuller@gmx.fr>
2022-02-22 14:44:32 +0100

    feat: :sparkles: SMTP

--- src/main/java/fr/epsi/clinic/ClinicApplication.java
+++ src/main/java/fr/epsi/clinic/ClinicApplication.java
@@ -13,7 +13,6 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestParam;
 
 @SpringBootApplication
-@Controller
 public class ClinicApplication {
 
 	public static void main(String[] args) {
---
+++ src/main/java/fr/epsi/clinic/configuration/EmailServiceConfiguration.java
@@ -0,0 +1,110 @@
+package fr.epsi.clinic.configuration;
+
+import java.io.File;
+import java.util.Properties;
+
+import javax.mail.MessagingException;
+import javax.mail.internet.MimeMessage;
+
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.core.io.FileSystemResource;
+import org.springframework.mail.SimpleMailMessage;
+import org.springframework.mail.javamail.JavaMailSenderImpl;
+import org.springframework.mail.javamail.MimeMessageHelper;
+
+/**
+ * SMTP Email sender
+ * @see sendSimpleMessage()
+ * @see sendMessageWithAttachment()
+ */
+@Configuration
+public class EmailServiceConfiguration {
+
+    @Value("${spring.mail.host}")
+    private String host = "smtp.gmail.com";
+    
+    @Value("${spring.mail.port}")
+    private int port = 587;
+
+    @Value("${spring.mail.username}")
+    private String username = "epsi.mspr.508@gmail.com";
+
+    @Value("${spring.mail.password}")
+    private String password = "canxawluuulpfita";
+
+    @Value("${spring.mail.transport.protocol}")
+    private String transportProtocol = "smtp";
+
+    @Value("${spring.mail.properties.mail.smtp.auth}")
+    private String smtpAuth = "true";
+
+    @Value("${spring.mail.properties.mail.smtp.starttls.enable}")
+    private String smtSstarttls = "true";
+
+    @Value("${spring.mail.properties.debug}")
+    private String debug = "true";
+
+    private JavaMailSenderImpl mailSender;
+
+    /**
+     * Constructor
+     */
+    public EmailServiceConfiguration() {
+        this.mailSender = new JavaMailSenderImpl();
+        this.mailSender.setHost(host);
+        this.mailSender.setPort(port);
+
+        this.mailSender.setUsername(username);
+        this.mailSender.setPassword(password);
+
+        final Properties props = this.mailSender.getJavaMailProperties();
+        props.put("mail.transport.protocol", transportProtocol);
+        props.put("mail.smtp.auth", smtpAuth);
+        props.put("mail.smtp.starttls.enable", smtSstarttls);
+        props.put("mail.debug", debug);
+    }
+
+    /**
+     * Send a simple mail
+     * @param to email
+     * @param from email
+     * @param subject
+     * @param text content
+     */
+    public void sendSimpleMessage(String to, String from, String subject, String text) {
+        final SimpleMailMessage message = new SimpleMailMessage();
+        message.setFrom(from);
+        message.setTo(to);
+        message.setSubject(subject);
+        message.setText(text);
+        this.mailSender.send(message);
+    }
+
+    /**
+     * Send an email with a file
+     * @param to email
+     * @param subject
+     * @param from email
+     * @param text content
+     * @param attachmentFile file
+     */
+    public void sendMessageWithAttachment(String to, String from, String subject, String text, File attachmentFile) {
+        MimeMessage message = this.mailSender.createMimeMessage();
+
+        try {
+            final MimeMessageHelper helper = new MimeMessageHelper(message, true);
+            helper.setFrom(from);
+            helper.setTo(to);
+            helper.setSubject(subject);
+            helper.setText(text);
+    
+            final FileSystemResource file = new FileSystemResource(attachmentFile);
+            helper.addAttachment(file.getFilename(), file);
+    
+            this.mailSender.send(message);
+        } catch (MessagingException e) {
+            e.printStackTrace();
+        }
+    }
+}
